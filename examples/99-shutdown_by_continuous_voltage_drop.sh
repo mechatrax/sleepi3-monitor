b7b2dd21fbbaf37b8738afcd82161cf0ffd72b70
Masahiro Honda <honda@mechatrax.com>
2021-11-18 20:08:20 +0900

    Add history size to exported variables

--- examples/99-shutdown_by_continuous_voltage_drop.sh
+++ examples/99-shutdown_by_continuous_voltage_drop.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-
-WAIT=3
+#
+# To execute this file, set oheshot: false in monitor.yml
+#
+# EXAMPLE
+#
+# common:
+#   interval: 1
+# voltage:
+#   history_size: 3
+#   commands:
+#     - exec: /etc/sleepi3-monitor/voltage.d/
+#       condition: under
+#       threshold: 10000
+#       oneshot: false
+#
 
 source /usr/lib/sleepi3-monitor/parse_env.sh
 
-if [ ${#VOLTAGE[@]} -lt $WAIT ]
+if [ ${#VOLTAGE[@]} -lt $VOLTAGE_HISTSIZE ]
 then
   exit 0
 fi
 
-for i in `seq $WAIT`
+for i in `seq $VOLTAGE_HISTSIZE`
 do
   if [ ${VOLTAGE[$i]} -gt $THRESHOLD ]
   then
